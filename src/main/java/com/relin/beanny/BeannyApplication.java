2e950699503f35925661642e869443e89e9e54c8
relin404 <ahmadmashaal01@gmail.com>
2024-08-11 22:21:22 +0300

    initial commit

---
+++ src/main/java/com/relin/beanny/BeannyApplication.java
@@ -0,0 +1,13 @@
+package com.relin.beanny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeannyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeannyApplication.class, args);
+	}
+
+}
