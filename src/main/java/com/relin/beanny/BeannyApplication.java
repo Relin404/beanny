66ecfa7ab719ff9970b712266ae1c465b4d66e93
relin404 <ahmadmashaal01@gmail.com>
2024-08-17 13:04:07 +0300

    feat(main): add logger annotation to main driver class

--- src/main/java/com/relin/beanny/BeannyApplication.java
+++ src/main/java/com/relin/beanny/BeannyApplication.java
@@ -1,13 +1,14 @@
 package com.relin.beanny;
 
+
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log
 public class BeannyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BeannyApplication.class, args);
 	}
-
 }
